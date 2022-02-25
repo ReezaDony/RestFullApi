c9e86d63b706caa3ff24ef4562e5ba608828da4a
ReezaDony <donnyrhezha@gmail.com>
2022-02-25 21:44:56 +0700

    correction routes

--- routes/routes.js
+++ routes/routes.js
@@ -24,7 +24,7 @@ router.post('/post', async (req, res) => {
 router.get('/getAll', async (req, res) => {
     try {
         const data = await Model.find();
-        res.json(data)
+        res.json(data);
     } catch (error) {
         res.status(500).json({message: error.message});
     }
@@ -59,7 +59,7 @@ router.delete('/delete/:id', async (req, res) => {
     try {
         const id = req.params.id;
         const data = await Model.findByIdAndDelete(id);
-        res.send('Document with [$data.name] has been deleted..');
+        res.send('Document with ${data.name} has been deleted..');
     } catch (error) {
         res.status(400).json({message: error.message});
     }
