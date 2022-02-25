9584ecf7ff7172dde0a744024096c24bb17cc022
ReezaDony <donnyrhezha@gmail.com>
2022-02-25 19:26:16 +0700

    updated router patch

--- routes/routes.js
+++ routes/routes.js
@@ -1,26 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const Model = require('../models/model');
 
 module.exports = router;
 
 //post method
-router.post('/post', (req, res) => {
-    res.send('Post API')
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    });
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+
  });
 
 //get all method
-router.get('/getAll', (req, res) => {
-    res.send('Get All API')
+router.get('/getAll', async (req, res) => {
+    try {
+        const data = await Model.find();
+        res.json(data)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 });
 
 //get by id method
-router.get('/getOne/:id', (req, res) => {
-    res.send(req.params.id)
+router.get('/getOne/:id', async (req, res) => {
+    try {
+        const data = await Model.findById(req.params.id);
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 });
 
 //update by id method
-router.patch('/update/:id', (req, res) => {
-    res.send('Update by ID API')
+router.patch('/update/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await Model.findByIdAndUpdate(id, updatedData, options);
+        res.send(result);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
 });
 
 //delete by id method
