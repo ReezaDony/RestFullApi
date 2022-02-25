d5e0a441dd72e61a7a46a333eec13d5351fe7bed
ReezaDony <donnyrhezha@gmail.com>
2022-02-25 17:56:25 +0700

    setting up routes complete

--- routes/routes.js
+++ routes/routes.js
@@ -2,3 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = router;
+
+//post method
+router.post('/post', (req, res) => {
+    res.send('Post API')
+ });
+
+//get all method
+router.get('/getAll', (req, res) => {
+    res.send('Get All API')
+});
+
+//get by id method
+router.get('/getOne/:id', (req, res) => {
+    res.send('Get by ID API')
+});
+
+//update by id method
+router.patch('/update/:id', (req, res) => {
+    res.send('Update by ID API')
+});
+
+//delete by id method
+router.delete('/delete/:id', (req, res) => {
+    res.send('Delete by ID API')
+});
